ee61fe2b80a2b875d29789d9574f177de663cafc
jmp-software <jmp.prensa@gmail.com>
2024-12-03 22:59:14 -0300

    En la rama proyecto-final  Tu rama está actualizada con 'origin/proyecto-final'.
     Cambios a ser confirmados:
    	modificados:     js/global.js

--- js/global.js
+++ js/global.js
@@ -1,26 +1,21 @@
-// Check if the page being called is "index.html"
-
-if (window.location.pathname == '/index.html' || window.location.pathname == '/vintageware/') {
-    // Include the menu from '/pages/menu-1.html' for "index.html"
+// Chequea si viene de la página index.html
+if (window.location.pathname == '/index.html' || window.location.pathname == '/vintageware/' || window.location.pathname == '/vintageware/index.html' {
+     // Incluye el menú de '/pages/menu-1.html' para "index.html"
     fetch('./pages/menu-1.html')  
     .then(response => response.text())
     .then(data => {
         document.getElementById('navbar-placeholder').innerHTML = data;
     })
     .catch(error => console.error('Error fetching menu:', error));
-    
     console.log(`Entro por TRUE`)
-
 } else {
-     // Include the menu from '/pages/menu-2.html' for the rest of the pages
+     // Incluye el menú de '/pages/menu-2.html' para el esto de las páginas
     fetch('../pages/menu-2.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('navbar-placeholder').innerHTML = data;
     })
     .catch(error => console.error('Error fetching menu:', error));
-    
-    console.log(`Entro por ELSE`)
+     console.log(`Entro por ELSE`)
  }
-
  console.log(`Esto es el path: ${window.location.pathname}`)
