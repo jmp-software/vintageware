6f577505ea14d4bbb8bb5f2a5b20c7b233da2a9f
jmp-software <jmp.prensa@gmail.com>
2024-12-03 22:47:38 -0300

    En la rama proyecto-final  Tu rama está actualizada con 'origin/proyecto-final'.
     Cambios a ser confirmados:
    	modificados:     js/global.js

--- js/global.js
+++ js/global.js
@@ -9,15 +9,13 @@ if (window.location.pathname == '/index.html') {
     })
     .catch(error => console.error('Error fetching menu:', error));
 } else {
-    /*
-    // Include the menu from '/pages/menu-2.html' for the rest of the pages
-    fetch('./pages/menu-2.html')
+     // Include the menu from '/pages/menu-2.html' for the rest of the pages
+    fetch('../pages/menu-2.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('navbar-placeholder').innerHTML = data;
     })
     .catch(error => console.error('Error fetching menu:', error));
-    */
-}
+ }
 
 console.log(`Esto es el path: ${window.location.pathname}`)
